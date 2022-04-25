185b662c7a0a8cdde6be60eee51c62c321de3780
grgvn <gregoiregavin@gmail.com>
2022-04-25 22:03:46 +0200

    Initial commit

---
+++ client/main.js
@@ -0,0 +1,6 @@
+import '../imports/ui/App.js';
+
+Meteor.startup(() => {
+    // Utilisé pour peupler la TasksCollection dans tuto : https://blaze-tutorial.meteor.com/simple-todos/02-collections.html
+    // A garder, peut-être utile pour initialiser des choses au lancement de l'app
+});
