1f22596508b46db9c357cded32029e62f86c93d5
Adriencoulonunil <adrien.coulon@unil.ch>
2022-04-30 16:04:10 +0200

    enlever check

--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -4,14 +4,14 @@ import { TasksCollection } from '../api/TasksCollection';
 
 import './Task.html';
 
-Template.task.events({
-  'click .toggle-checked'() {
+//Template.task.events({
+//  'click .toggle-checked'() {
     // Set the checked property to the opposite of its current value
-    TasksCollection.update(this._id, {
-      $set: { isChecked: !this.isChecked },
-    });
-  },
-  'click .delete'() {
-    TasksCollection.remove(this._id);
-  },
-});
+//    TasksCollection.update(this._id, {
+//      $set: { isChecked: !this.isChecked },
+//    });
+ // },
+//  'click .delete'() {
+//    TasksCollection.remove(this._id);
+//  },
+//});
