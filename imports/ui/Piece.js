0ce72313a54609d1506376f21ab08e4160c02929
Adriencoulonunil <adrien.coulon@unil.ch>
2022-05-03 13:10:00 +0200

    mmm

--- imports/ui/Piece.js
+++ imports/ui/Piece.js
@@ -5,12 +5,12 @@ import { PieceCollection } from '../api/PieceCollection';
 import './Piece.html';
 
 Template.piece.events({
- // 'click .toggle-checked'() {
+  'click .toggle-checked'() {
     // Set the checked property to the opposite of its current value
- //   PieceCollection.update(this._id, {
- //     $set: { isChecked: !this.isChecked },
-//    });
-//  },
+    PieceCollection.update(this._id, {
+      $set: { isChecked: !this.isChecked },
+    });
+  },
   'click .delete'() {
     PieceCollection.remove(this._id);
   },
