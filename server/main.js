0bf69415f3353b3710601f7e676198a38f4240be
grgvn <gregoiregavin@gmail.com>
2022-05-08 16:03:47 +0200

    body + header styling

--- server/main.js
+++ server/main.js
@@ -1,5 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
+
 Meteor.startup(() => {
-    // code to run on server at startup
+    if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+        Accounts.createUser({
+          username: SEED_USERNAME,
+          password: SEED_PASSWORD,
+        });
+      }
 });
