d4963c3d732e21c2b7e3b97f91abf12874d22e8d
bcseverine <55986983+bcseverine@users.noreply.github.com>
2022-05-11 11:29:09 +0200

    Tentative 1
    Tentative de faire afficher les pièces. Apparaissent-elles vraiment dans la base de données ?

--- imports/ui/ecrans/dashboard/dashboard.js
+++ imports/ui/ecrans/dashboard/dashboard.js
@@ -1,3 +1,10 @@
 import './dashboard.html'
 import '../../components/header/header'
 import '../../components/footer/footer'
+import { PieceCollection } from '../../../collections/Pieces'
+
+Template.piece.helpers({
+    nomPiece(){
+        return PieceCollection.find({});
+    }
+});
