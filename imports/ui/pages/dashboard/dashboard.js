fef772b6c8b3d4451e2860a3ec12eab5985b33fa
Adriencoulonunil <adrien.coulon@unil.ch>
2022-05-11 09:39:10 +0200

    up

--- imports/ui/pages/dashboard/dashboard.js
+++ imports/ui/pages/dashboard/dashboard.js
@@ -1,3 +1,15 @@
 import './dashboard.html'
 import '../../components/header/header'
 import '../../components/footer/footer'
+
+Template.dashboard.events({
+    "submit .js-ajouter-piece"(event){
+        event.preventDefault();
+       const nomPiece = event.target.text.value
+
+        Pieces.insert({
+            nom: nomPiece,
+            dateCreation: new Date(),
+          }); 
+    }
+})
