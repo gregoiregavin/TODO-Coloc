b4589dee12e98b6d94a9322070bedb4149f220cc
bcseverine <55986983+bcseverine@users.noreply.github.com>
2022-05-30 20:46:04 +0200

    Leaderboard
    Refaire la route du leaderboard
    + HTML /js du leaderboard, j'ai pas réussi à créer le deuxième template faudra que je repasse dessus

--- imports/ui/ecrans/leaderboard/leaderboard.js
+++ imports/ui/ecrans/leaderboard/leaderboard.js
@@ -1,4 +1,17 @@
 import './leaderboard.html'
 import '../../components/header/header'
 import '../../components/footer/footer'
+import { Colocations } from '../../../collections/Colocations';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 
+Template.leaderboard.helpers({
+    nomColoc() {
+      return Colocations.findOne({ _id: FlowRouter.getParam('colocId') }).nom;
+    },
+});
+
+// Template.membre.helpers({
+//     membres(){
+//         return Colocations.find({ colocId: FlowRouter.getParam('colocId') }).membres;
+//     },
+// });
