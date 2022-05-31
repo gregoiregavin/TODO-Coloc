f08d8c531e0dac720d5f81ab96b2f17286a77057
bcseverine <55986983+bcseverine@users.noreply.github.com>
2022-05-31 11:13:30 +0200

    : pour variable

--- imports/ui/components/nav/nav.js
+++ imports/ui/components/nav/nav.js
@@ -6,12 +6,12 @@ import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 Template.nav.events({
     "click #leaderboard"() {
         Meteor.score(
-            FlowRouter.go('/leaderboard/id')
+            FlowRouter.go('/leaderboard/:colocId')
         );
     },
     "click #dashboard"() {
         Meteor.dashboard(
-            FlowRouter.go('/colocation/colocId')
+            FlowRouter.go('/colocation/:colocId')
         );
     },
 });
