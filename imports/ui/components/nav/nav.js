ded330325ea53d646b0c9b7a12410ee0d892b3fa
Adriencoulonunil <adrien.coulon@unil.ch>
2022-05-31 08:45:16 +0200

    nav

--- imports/ui/components/nav/nav.js
+++ imports/ui/components/nav/nav.js
@@ -1,2 +1,17 @@
 import './nav.html'
 import './nav.css'
+
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
+
+Template.nav.events({
+    "click #leaderboard"() {
+        Meteor.score(
+            FlowRouter.go('leaderboard/:id')
+        );
+    },
+    "click #dashboard"() {
+        Meteor.dashboard(
+            FlowRouter.go('colocation/:colocId')
+        );
+    },
+});
