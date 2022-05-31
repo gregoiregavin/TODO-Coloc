bd23c0137d1903776dbe92fbc1fc2117a79aaed4
Adriencoulonunil <adrien.coulon@unil.ch>
2022-05-31 20:33:08 +0200

    Nav.js (score)

--- imports/ui/components/nav/nav.js
+++ imports/ui/components/nav/nav.js
@@ -1,15 +1,20 @@
 import "./nav.html";
 import "./nav.css";
 
+
 import { FlowRouter } from "meteor/ostrio:flow-router-extra";
 
 Template.nav.events({
   "click #leaderboard"() {
-    FlowRouter.go("/leaderboard/:colocId");
-    console.log("CLICK");
+    const colocId = FlowRouter.getParam("colocId")
+    FlowRouter.go(
+      "leaderboard", { colocId: colocId }
+    )
   },
   "click #dashboard"() {
-    FlowRouter.go("/colocation/:colocId");
-    console.log("CLICK");
-  },
+    const colocId = FlowRouter.getParam("colocId")
+    FlowRouter.go(
+      "coloc", { colocId: colocId }
+    )
+  }
 });
